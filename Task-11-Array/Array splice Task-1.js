1bf18da822a2f90c260a287c43441663660345d4
LAKSHMIVARAPRASAD-prasad <137158768+LAKSHMIVARAPRASAD-prasad@users.noreply.github.com>
2024-03-10 16:43:35 +0530

    Add files via upload

---
+++ Task-11-Array/Array splice Task-1.js
@@ -0,0 +1,22 @@
+let fruits=[]
+console.log (fruits);
+fruits.unshift("mango")
+console.log (fruits);
+fruits.unshift("kiwi")
+console.log (fruits);
+fruits.unshift("cherry")
+console.log (fruits);
+fruits.push("papaya")
+console.log (fruits);
+fruits.push("pineapple")
+console.log (fruits);
+fruits.push("banana")
+console.log (fruits);
+fruits.shift()
+console.log (fruits);
+fruits.shift()
+console.log (fruits);
+fruits.pop()
+console.log (fruits);
+fruits.pop()
+console.log (fruits);
