7b7e52c7b7d87c991ee530316ab36ba3df139aef
LAKSHMIVARAPRASAD-prasad <137158768+LAKSHMIVARAPRASAD-prasad@users.noreply.github.com>
2024-03-10 16:16:49 +0530

    Add files via upload

---
+++ Task-10 External-Js/Ps-1-Addition-Js.js
@@ -0,0 +1,6 @@
+function AddtionOf2Numbers(n1,n2)
+            {
+            let result
+            result = (n1+n2)
+            console.log(result)
+            }
---
+++ Task-10 External-Js/Ps-2-Simple Interest-Js.js
@@ -0,0 +1,6 @@
+function SimpleInterest(p,t,r)
+            {
+            let result
+            result = (p*r*t)/100
+            console.log(result)
+            }
